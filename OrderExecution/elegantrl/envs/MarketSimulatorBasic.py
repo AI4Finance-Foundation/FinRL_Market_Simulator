46b8d081fbc7f1764bf3a64c16cc82ba44f43bc9
YonV1943 曾伊言 <37322666+Yonv1943@users.noreply.github.com>
2023-02-17 10:33:52 +0800

    :construction: order execution policy

--- OrderExecution/elegantrl/envs/MarketSimulatorBasic.py
+++ OrderExecution/elegantrl/envs/MarketSimulatorBasic.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numpy.random as rd
 import matplotlib.pyplot as plt
-from typing import Union
+from typing import Union, Tuple
 
 Array = np.ndarray
 
@@ -10,16 +10,14 @@ class FakeMarketSimulator:
     def __init__(self):
         size = 512
 
-        close_prices = get_random_array(initial_value=100, size=size, drift_rate=0.01, volatility=0.1)
+        close_prices = 100 * generate_brownian_motion_ary(size=size, drift_rate=0.01, volatility=0.1)
         self.close_prices = close_prices.round(2)
 
-        volumes = get_random_array(initial_value=1000, size=size, drift_rate=0.01, volatility=0.1)
+        volumes = 1000 * np.exp(generate_arima_noise(ar_params=(0.2, -0.3), ma_params=(0.4, 0.1), size=size))
         self.volumes = np.abs(volumes)
 
-        # map_to_executed_volume =
         x_data = np.linspace(start=0, stop=2, num=32)
         y_data = np.exp(-x_data ** 2)  # normal distribution Probability density function (Gaussian integral)
-
         self.fit_plot_nums = np.polyfit(x_data, y_data, deg=5)
 
         self.t = 0
@@ -47,19 +45,51 @@ class FakeMarketSimulator:
         return y_eval
 
 
-def get_random_array(initial_value: float, size: int, drift_rate: float, volatility: float):
+def generate_brownian_motion_ary(
+        size: int = 128,
+        drift_rate: float = 0.01,
+        volatility: float = 0.1
+) -> Array:
     dt = 1 / size
     delta = rd.normal(loc=(drift_rate - 0.5 * volatility ** 2) * dt, scale=volatility * np.sqrt(dt), size=size)
-    return initial_value * np.exp(delta.cumsum())
+    return np.exp(delta.cumsum())
+
+
+def generate_arima_noise(
+        size: int = 128,
+        ar_params: Tuple[float, float] = (0.2, -0.3),
+        ma_params: Tuple[float, float] = (0.4, 0.1),
+) -> Array:  # autoregressive integrated moving average
+    errors = rd.normal(size=size)
+    y = np.zeros(size)
+    for i in range(max(len(ar_params), len(ma_params)), size):
+        ar_term = np.dot(ar_params, y[i - len(ar_params):i][::-1])
+        ma_term = np.dot(ma_params, errors[i - len(ma_params):i][::-1])
+        y[i] = ar_term + ma_term + errors[i]
+    return y
+
+
+# class IcebergPolicy:
+#     def __init__(self):
+        
+
+'''tutorial'''
 
 
 def tutorial__get_random_array():
-    array = get_random_array(initial_value=100, size=512, drift_rate=0.01, volatility=0.1)
-    plt.plot(array)
+    size = 256
+
+    array = 100 * (generate_brownian_motion_ary(size=size, drift_rate=0.01, volatility=0.1) - 1)
+    plt.plot(array, label='100*(brownian_motion-1)')
+    array = generate_arima_noise(ar_params=(0.2, -0.3), ma_params=(0.4, 0.1), size=size)
+    plt.plot(array, label='arima_noise')
+    array = np.exp(-1 + generate_arima_noise(size=size, ar_params=(0.2, -0.3), ma_params=(0.4, 0.1)))
+    plt.plot(array, label='exp(arima_noise)')
+
+    plt.title('generate random noise')
+    plt.legend()
+    plt.grid()
     plt.show()
-
-
-"""ploy fix"""
 
 
 def tutorial__get_fix_ploy_nums():
@@ -78,10 +108,14 @@ def tutorial__get_fix_ploy_nums():
     plt.show()
 
 
+'''run'''
+
+
 def run():
     env = FakeMarketSimulator()
 
 
 if __name__ == '__main__':
-    run()
+    # run()
     # tutorial__get_fix_ploy_nums()
+    tutorial__get_random_array()
